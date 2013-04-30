a7d586f3aef16ccb1310d121e5200531abfc18c9
vholiveira <vholiveira23@gmail.com>
2013-04-30 00:15:15 -0300

    Create eval.c

---
+++ eval.c
@@ -0,0 +1,15 @@
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+void apply_op(op_t, tree_t l, tree_t r, tree_t result){
+printf("Essa funcao aplica operacoes sobre duas primitivas");
+}
+void eval_tree(tree_t, int x ,y ){
+printf("Essa funcao avalia a arvore (faz uniao, interseccao ou diferença)");
+}
+void create_tree(transprim_t){
+printf("Essa função cria uma arvore");
+}
+void compose_tree(trans_t, tree_t){
+printf("Essa função adiciona uma transformacao para a arvore");
+}
